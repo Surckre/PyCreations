fae7208aefec2b9fd3668e5e104b689ec83770db
Surckre <NotMeLuc@gmail.com>
2024-10-02 22:52:28 +0200

    Create counter.py

---
+++ counter.py
@@ -0,0 +1,4 @@
+y = int(input("Type a Number: ")) # input to type how much it have to count
+for x in range(0, y+1): # function
+    print(x) # print the numbers
+input("Press Enter...") # prevent automatic closing console
