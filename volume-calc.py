bebd29fe1c9175af9919d35918f64ac64c94dc97
Surckre <NotMeLuc@gmail.com>
2024-10-15 11:23:27 +0200

    Create volume-calc.py

---
+++ volume-calc.py
@@ -0,0 +1,7 @@
+lenght = float(input("Enter the lenght: "))
+width = float(input("Enter the width: "))
+heigth = float(input("Enter the height: "))
+
+volume = lenght * width * heigth
+
+print(volume)
